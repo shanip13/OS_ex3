a75d4ea8bafa81e3ef089dde202f4f22c6b4bfad
yaala <yaala@gmail.com>
2024-07-06 17:20:07 +0300

    created structs

---
+++ MapReduceFramework.cpp
@@ -0,0 +1,14 @@
+#include "MapReduceFramework.h"
+#include <atomic>
+
+typedef struct {
+    JobState job_state;
+    const InputVec& inputVec; // TODO maybe change &
+    OutputVec& outputVec;
+    std::atomic<int>* atomic_counter;
+} ClientContext;
+
+typedef struct {
+    ClientContext client_context;
+    IntermediateVec& intermediate_vec;
+} ThreadContext;
