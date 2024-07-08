410ff1136bc620b9a6b27bd633c8261a41a1cc7b
yaala <yaala@gmail.com>
2024-07-08 13:38:53 +0300

    finished shuffle and reduce

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -19,6 +19,7 @@ typedef struct {
     // vectors
     const InputVec& inputVec;
     std::vector<IntermediateVec> intermediate_vecs;
+    std::vector<IntermediateVec> shuffled_queue;
     OutputVec& outputVec;
     // locks
     std::atomic<int>* atomic_counter;
@@ -37,6 +38,43 @@ bool compareIntermediatePair(const IntermediatePair& pair1, const IntermediatePa
   return pair1.first < pair2.first;
 }
 
+bool K2_equals(K2* key1, K2* key2) {
+  return (not (*key1 < *key2)) && (not (*key2 < *key1));
+}
+
+int shuffle(std::vector<IntermediateVec> &intermediate_vecs,
+            std::vector<IntermediateVec> &shuffled_queue) {
+  while (not intermediate_vecs.empty()) {
+    // delete empty vectors
+    auto it = intermediate_vecs.begin();
+    while (it != intermediate_vecs.end()) {
+      if (it->empty()) { it = intermediate_vecs.erase(it);}
+      else {++it;}
+    }
+    if (intermediate_vecs.empty()) {break;}
+
+    // find the largest key
+    K2* curr_key;
+    K2* largest_key = intermediate_vecs[0][0].first;
+    for (auto& vec : intermediate_vecs) {
+      curr_key = vec.back().first;
+      if (*largest_key < *curr_key) {
+        *largest_key = *curr_key;
+      }
+    }
+
+    // pop elements with the largest key into new sequence
+    shuffled_queue.push_back(*(new IntermediateVec()));
+    for (auto& vec : intermediate_vecs) {
+      while (!vec.empty() && K2_equals(vec.back().first, largest_key)) {
+        shuffled_queue.back().push_back(vec.back());
+        vec.pop_back();
+      }
+    }
+  }
+  return 0;
+}
+
 void* thread_entry_point(void *arg) {
   ThreadContext* thread_context = (ThreadContext*) arg;
   ClientContext* client_context = thread_context->client_context;
@@ -50,6 +88,7 @@ void* thread_entry_point(void *arg) {
     curr_pair = &(client_context->inputVec[prev_count]);
     client_context->client.map(curr_pair->first, curr_pair->second,
                            thread_context);
+    // TODO map uses emit2 to put results into thread_context->intermediatevec
   }
 
   // sort phase
@@ -64,6 +103,7 @@ void* thread_entry_point(void *arg) {
   // shuffle phase
   if (thread_context->thread_id == 0) {
     std::cout << "Thread " << thread_context->thread_id << " shuffle phase\n";
+    shuffle(client_context->intermediate_vecs, client_context->shuffled_queue);
     sem_post(&client_context->shuffle_semaphore);
     client_context->atomic_counter->store(0);
   }
@@ -74,7 +114,14 @@ void* thread_entry_point(void *arg) {
   std::cout << "Thread " << thread_context->thread_id << " reducing phase\n";
 
   // reduce phase
-
+  IntermediateVec* curr_vec;
+  while ((prev_count = (client_context->atomic_counter->fetch_add(1))) <
+         client_context->shuffled_queue.size()) {
+    std::cout << "Thread " << thread_context->thread_id << " map phase\n";
+    curr_vec = &(client_context->shuffled_queue[prev_count]);
+    client_context->client.reduce(curr_vec, thread_context);
+    // TODO reduce uses emit3 to put results into client_context->outputvec
+  }
 
   pthread_exit(NULL);
 }
@@ -86,16 +133,20 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
 
   // define client_context
   ClientContext* client_context = new ClientContext {
-    client,
-    {UNDEFINED_STAGE, 0},
-    pthread_self(),
-    inputVec,
-    *(new std::vector<IntermediateVec>(multiThreadLevel)),
-    outputVec,
-    new std::atomic<int>(0),
-    &barrier,
-    sem_t(),
-    PTHREAD_MUTEX_INITIALIZER
+    // general
+    client, // client
+    {UNDEFINED_STAGE, 0}, // job_state
+    pthread_self(), // main_thread
+    // vectors
+    inputVec, // inputVec
+    *(new std::vector<IntermediateVec>(multiThreadLevel)), // intermediate_vecs
+    *(new std::vector<IntermediateVec>()), // shuffle_queue
+    outputVec, // outputVec
+    // locks
+    new std::atomic<int>(0), // atomic_counter
+    &barrier, // barrier
+    sem_t(), // shuffle_semaphore
+    PTHREAD_MUTEX_INITIALIZER // vector_mutex
   };
   sem_init(&(client_context->shuffle_semaphore), 0, 0);
 
@@ -106,9 +157,9 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
   for(int i = 0; i < multiThreadLevel; i++) {
     printf("Creating thread %d\n", i);
     thread_context = new ThreadContext {
-      i,
-      client_context,
-      client_context->intermediate_vecs[i]
+      i, // thread_id
+      client_context, // client_context
+      client_context->intermediate_vecs[i] // intermediate_vec
     };
     ret = pthread_create(&threads[i], NULL, thread_entry_point, thread_context);
     if (ret) {
@@ -116,9 +167,12 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
       exit(-1);
     }
   }
+
+  // wait for all threads to finish
   for (int i = 0; i < multiThreadLevel; ++i) {
     pthread_join(threads[i], NULL);
   }
+
   int i = *(client_context->atomic_counter);
   printf("%d\n", i);
   printf("bye\n");
